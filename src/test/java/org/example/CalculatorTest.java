712387c74b6f74ceaa3356cb6dc0dac27157511d
FatjonaCeni <FatCen18@htl-shkoder.com>
2025-01-17 12:20:50 +0100

    first commit

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,43 @@
+package org.example;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void add() {
+        assertEquals(7,calculator.add(4,3));
+        assertNotEquals(7, calculator.add(3,3));
+    }
+
+    @Test
+    void sub() {
+        assertEquals(2, calculator.sub(4, 2));
+        assertNotEquals(2, calculator.sub(5, 2));
+    }
+
+    @Test
+    void multi() {
+        assertEquals(10, calculator.multi(2, 5));
+        assertNotEquals(2, calculator.multi(5, 2));
+    }
+
+    @Test
+    void divide() {
+        assertEquals(2, calculator.divide(4, 2));
+        assertNotEquals(5, calculator.divide(15, 2));
+    }
+    @Test
+    void divideByZero(){
+        assertThrows(IllegalArgumentException.class,()-> calculator.divide(10, 0));
+
+    }
+}
